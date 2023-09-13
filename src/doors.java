049d9e401e0c536ae64076bfc93df0e5fe9f4953
petitoa <141281325+petitoa@users.noreply.github.com>
2023-09-13 19:12:26 -0400

    doors
    finished doors and doorsMain

--- src/doors.java
+++ src/doors.java
@@ -1,5 +1,5 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class doors {
@@ -8,19 +8,75 @@ public class doors {
         int goat = 0;
         int car = 1;
 
-        Random random = new Random();
-
+        // add 2 goats and car to list
+        doors.add(goat);
+        doors.add(goat);
         doors.add(car);
 
-        doors.add(random.nextInt(3), goat);
-        doors.add(random.nextInt(3), goat);
+        // Shuffle the doors to randomize their positions
+        Collections.shuffle(doors);
 
         return doors;
     }
 
-    public boolean doorChanged(ArrayList<Integer> doors){
-        return null;
+    public double doorChanged() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        final int ITERATIONS = 10000;
+        for (int i = 0; i < ITERATIONS; i++) {
+            ArrayList<Integer> doorsChanged = new ArrayList<>(assignGoatsAndCarDoors());
+            Random random = new Random();
+
+            //Randomly choose door and remove so it's not opened next.
+            int chosenDoor1 = doorsChanged.get(random.nextInt(3));
+            doorsChanged.remove(chosenDoor1);
+
+            //"Open" a random door that does not contain the car. Remove, so it can't be chosen.
+            int goatIndex = doorsChanged.indexOf(0);
+            if (goatIndex != -1) {
+                doorsChanged.remove(goatIndex);
+            }
+
+            //Switch to last option
+            int chosenDoor2 = doorsChanged.get(0);
+
+            if (chosenDoor2 == 1) {
+                results.add(chosenDoor2);
+            }
+
+        }
+
+        int wins = 0;
+        for (int element : results) {
+            wins += element;
+        }
+
+        return ((double) wins / ITERATIONS) * 100;
     }
 
-    public boolean doornotChanged()
+    public double doorNotChanged() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        final int ITERATIONS = 10000;
+        for (int i = 0; i < ITERATIONS; i++) {
+            ArrayList<Integer> doorsNotChanged = new ArrayList<>(assignGoatsAndCarDoors());
+            Random random = new Random();
+
+            //Randomly choose door because it's always P(1/3)
+            int chosenDoor = doorsNotChanged.get(random.nextInt(3));
+
+            if (chosenDoor == 1) {
+                results.add(chosenDoor);
+            }
+
+        }
+
+        int wins = 0;
+        for (int element : results) {
+            wins += element;
+        }
+
+        return ((double) wins / ITERATIONS) * 100;
+    }
+
 }
--- src/doorsMain.java
+++ src/doorsMain.java
@@ -1,14 +1,28 @@
-import java.util.ArrayList;
+/**
+ * Alexis Petito
+ * Question A.)
+ * E1 = nice prize, E2 = Dud1, E3 = Dud3
+ * P(E1) = 1/3, P(E2) = 1/3, P(E3) = 1/3
+ * Probability that the contestant selects curtain hiding nice prize = P(E1) = 1/3
+ *
+ * Question B.)
+ * In part a it is shown that the probability of the good prize is 1/3
+ * In the event that one Dud is removed and you are asked to choose again the probability would become
+ * E1 = nice prize, E2 = Dud1, E3 = Dud3
+ * P(E1) = 1/3, P(E2) = 1/3, P(E3) = 1/3
+ * EX.) P(E1) + P(E2) = 2/3 because the game show host removing a dud acts as another pick
+ * if you switch the curtain the probability becomes 2/3, so switching the doors is the best choice.
+ */
+
 
 public class doorsMain {
-    //3 doors one had car 2 had goats
-    // answer questions a to b in comments of code
     public static void main(String[] args) {
         doors test = new doors();
 
-        String result = String.valueOf(doors.assignGoatsAndCarDoors());
+        double doorNotChangedResult = test.doorNotChanged();
+        double doorChangedResult = test.doorChanged();
 
-        System.out.println(result);
+        System.out.println("The percentage of wins when you don't switch doors : " + doorNotChangedResult + "%" + "\n" + "The percentage of wins when you switch doors : " + doorChangedResult + "%");
 
 
     }
