a07e0aee459f25b3985a2127400f1c51048507f4
Alexis <>
2023-09-22 13:45:24 -0400

    Midterm Project Start

---
+++ src/Factory.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.BufferedWriter;
+
+//when choosing random numbers skew the miles in car
+public class Factory {
+
+    ArrayList<String> carType = new ArrayList<>();
+    ArrayList<String> color = new ArrayList<>();
+    Random random = new Random();
+
+    public Factory(){
+        //Add car types to array
+        carType.add("Hatchback");
+        carType.add("SUV");
+        carType.add("Sedan");
+        carType.add("Sedan");
+        carType.add("Truck");
+        carType.add("Coupe");
+        carType.add("Convertible");
+
+        //Add colors to array
+        color.add("Red");
+        color.add("Red");
+        color.add("Blue");
+        color.add("Yellow");
+        color.add("White");
+        color.add("Black");
+        color.add("Cream");
+
+    }
+
+    public int getRandomYear(){
+        return random.nextInt(1973,2023 + 1);
+    }
+
+    public int getRandomMiles(){
+        return random.nextInt(0, 250000 + 1);
+    }
+
+    public String getRandomCarType(){
+        return carType.get(random.nextInt(carType.size() +1));
+    }
+
+    public String getRandomColor(){
+        return color.get(random.nextInt(color.size() + 1));
+    }
+    ArrayList<Car> createCar(int numOfCars) {
+        ArrayList<Car> cars = new ArrayList<>();
+        for (int i = 0; i < numOfCars; i++) {
+            String carType = getRandomCarType();
+            int year = getRandomYear();
+            String color = getRandomColor();
+            int miles = getRandomMiles();
+            cars.add(new Car(carType, year, color, miles));
+        }
+        return cars;
+    }
+
+    def writeToCsv(){
+
+    }
+}
---
+++ src/TestFactory.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class TestFactory {
+    Factory test = new Factory();
+
+
+}
